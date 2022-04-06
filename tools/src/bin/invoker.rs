ee222efbd2660ad26cc6ee07c4f22d6a26b7060b
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-04-06 17:24:50 +0200

    write invoker tool

---
+++ tools/src/bin/invoker.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+use common::{
+    invoke::{Invocation, InvocationContext, InvocationType::Json, JsonResponse},
+    ipc::UnixClient,
+    prelude::*,
+};
+use log::*;
+#[derive(Parser, Debug)]
+struct Flags {
+    #[clap(short, long)]
+    owner: String,
+    #[clap(short, long)]
+    app_name: String,
+    #[clap(short, long)]
+    payload: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    simple_logger::init()?;
+    let flags = Flags::try_parse()?;
+    let stream = tokio::net::UnixStream::connect("/tmp/zhur-engine.sck").await?;
+    let mut client = UnixClient::new(1024 * 8, stream);
+    let names = vec!["Alice".to_string(), "Bob".to_string(), "Carol".to_string()];
+    for name in names {
+        let ctx = InvocationContext::new(flags.owner.clone(), flags.app_name.clone(), Json);
+        let invocation = Invocation::new(ctx, &name);
+        let response = client.request::<_, JsonResponse>(&invocation).await?;
+        info!("Got response from engine:\n{}", response.payload);
+    }
+    Ok(())
+}
