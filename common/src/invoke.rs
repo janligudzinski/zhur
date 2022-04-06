60efead6d1a59d83bed9f7dff7662a7e7e3166a0
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-04-06 16:31:56 +0200

    wrote invoke module

---
+++ common/src/invoke.rs
@@ -0,0 +1,51 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+/// App invocation types. This decides how an app's response is processed.
+#[derive(Deserialize, Serialize, Debug)]
+pub enum InvocationType {
+    /// (Re)serialize whatever the app returns as JSON.
+    Json,
+}
+
+/// Execution data concerning a particular invocation.
+#[derive(Deserialize, Serialize)]
+pub struct InvocationContext {
+    /// Whose app we're invoking.
+    pub owner: String,
+    /// Which app it is.
+    pub app: String,
+    /// When the app was invoked.
+    pub timestamp: DateTime<Utc>,
+    /// What type of invocation we're dealing with.
+    inv_type: InvocationType,
+}
+impl InvocationContext {
+    pub fn new(owner: String, app: String, inv_type: InvocationType) -> Self {
+        Self {
+            owner,
+            app,
+            timestamp: Utc::now(),
+            inv_type,
+        }
+    }
+}
+/// An invocation sent to the app engine, containing an arbitrary serializable payload.
+#[derive(Deserialize, Serialize)]
+pub struct Invocation {
+    pub ctx: InvocationContext,
+    pub payload: Vec<u8>,
+}
+impl Invocation {
+    pub fn new<P: Serialize>(ctx: InvocationContext, payload: &P) -> Self {
+        Self {
+            ctx,
+            payload: bincode::serialize(payload).expect("Could not serialize invocation payload."),
+        }
+    }
+}
+/// An arbitrary JSON response from the app engine. Currently the only supported type.
+pub struct JsonResponse {
+    pub ctx: InvocationContext,
+    pub payload: String,
+}
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,5 +1,7 @@
 /// Error types.
 pub mod errors;
+/// App invocation-related types.
+pub mod invoke;
 /// Module for IPC communication between Zhur modules.
 pub mod ipc;
 // Reexports for other crates.
