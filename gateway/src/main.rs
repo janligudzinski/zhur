bbf476748be935a6441f900c7667a1d8c7e499c2
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-05-06 15:49:34 +0200

    the gateway now calls the engine

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,20 +1,46 @@
-use std::{net::SocketAddr, str::FromStr};
+use std::{net::SocketAddr, os::linux::raw, str::FromStr};
 
-use common::prelude::{log::*, tokio};
+use common::{
+    invoke::{Invocation, InvocationContext, InvocationResponse},
+    prelude::{log::*, tokio},
+};
 
 use axum::{extract::Path, routing::any, Router};
+use ipc::UnixClient;
 
-async fn invoke(Path((owner, app, raw_path)): Path<(String, String, Option<String>)>) -> String {
+async fn invoke_text(owner: String, app: String, payload: String) -> anyhow::Result<String> {
+    let stream = tokio::net::UnixStream::connect("/tmp/zhur-engine.sck").await?;
+    let mut client = UnixClient::new(1024 * 8, stream);
+    let invocation = Invocation::TextInvocation {
+        ctx: InvocationContext::new(owner, app),
+        payload,
+    };
+    let response = client.request::<_, InvocationResponse>(&invocation).await?;
+    match response {
+        InvocationResponse::TextResponse { ctx: _, payload } => {
+            info!("Got response from engine:\n{}", &payload);
+            Ok(payload)
+        }
+        _ => panic!("Unsupported response type. Expected text response."),
+    }
+}
+
+async fn text_invoke_handler(
+    Path((owner, app, raw_path)): Path<(String, String, Option<String>)>,
+) -> String {
     let raw_path = raw_path.unwrap_or("/".to_string());
-    let res = format!("Invoking {}:{} with path of {}", owner, app, raw_path);
+    let res = format!("Invoking {}:{} with payload of {}", owner, app, raw_path);
     info!("{}", &res);
-    res
+    match invoke_text(owner, app, raw_path).await {
+        Ok(x) => x,
+        Err(e) => format!("Error: {e}"),
+    }
 }
 
 #[tokio::main]
 async fn main() {
     simple_logger::init().unwrap();
-    let app = Router::new().route("/:owner/:app/*raw_path", any(invoke));
+    let app = Router::new().route("/:owner/:app/*raw_path", any(text_invoke_handler));
     let server = axum::Server::bind(&SocketAddr::from_str("127.0.0.1:8000").unwrap())
         .serve(app.into_make_service());
 
