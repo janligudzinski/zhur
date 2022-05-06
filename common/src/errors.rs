c9c3a7aab712ced4ef5d60b747c7553866c07981
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-05-06 15:37:26 +0200

    new error type

--- common/src/errors.rs
+++ common/src/errors.rs
@@ -22,4 +22,10 @@ pub enum IpcError {
 pub enum InvocationError {
     #[error("The HTTP request being used to create an invocation was malformed.")]
     BadHttpRequest,
+    #[error("The application returned an internal error: {0}")]
+    ApplicationError(String),
+    #[error(
+        "No application named {0}:{1} was found. It may not exist or be temporarily disabled."
+    )]
+    NoAppFound(String, String),
 }
