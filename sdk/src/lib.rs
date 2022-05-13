e9cbc531899b3db763590bb97455111e62abf9f7
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-05-13 11:34:32 +0200

    hide reexported crates

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,21 +1,24 @@
-pub use bincode;
-pub use wapc_guest;
+/// This module contains crates the Zhur SDK uses, but you should not directly have to import.
+pub mod __internals {
+    pub use bincode;
+    pub use wapc_guest;
+}
 /// This macro sets up all the Zhur SDK boilerplate for a text function that takes a UTF-8 `String` and returns another.
 #[macro_export]
 macro_rules! text_function {
     ($function_name:ident) => {
-        fn func_wrapper(msg: &[u8]) -> zhur_sdk::wapc_guest::CallResult {
-            let msg_string = zhur_sdk::bincode::deserialize::<String>(msg)
+        fn func_wrapper(msg: &[u8]) -> zhur_sdk::__internals::wapc_guest::CallResult {
+            let msg_string = zhur_sdk::__internals::bincode::deserialize::<String>(msg)
                 .expect("a text function expects a deserializable utf-8 string");
             let output = $function_name(msg_string);
-            let response = zhur_sdk::bincode::serialize(&output)
+            let response = zhur_sdk::__internals::bincode::serialize(&output)
                 .expect("should be able to bincode-serialize a string");
             Ok(response)
         }
         #[no_mangle]
         pub fn wapc_init() {
             std::panic::set_hook(Box::new(|p| {
-                zhur_sdk::wapc_guest::host_call(
+                zhur_sdk::__internals::wapc_guest::host_call(
                     "zhur",
                     "internals",
                     "panic",
@@ -23,7 +26,7 @@ macro_rules! text_function {
                 )
                 .unwrap();
             }));
-            zhur_sdk::wapc_guest::register_function("text", func_wrapper)
+            zhur_sdk::__internals::wapc_guest::register_function("text", func_wrapper)
         }
     };
 }
