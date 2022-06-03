76bf1e9b4f6f63b1c693b3739c5f513a5aac6659
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-03 15:42:21 +0200

    moved db server logic out to new module

--- db/src/main.rs
+++ db/src/main.rs
@@ -1,100 +1,35 @@
+use std::sync::Arc;
+
 use common::db::*;
 use common::prelude::*;
-use sled::Db;
 use tokio::net::UnixListener;
+
+mod data;
+use data::*;
+
 const DB_SOCKET_PATH: &str = "/tmp/zhur-db.sck";
 const DB_FILE_PATH: &str = "/tmp/zhur-db.sled";
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     simple_logger::init().unwrap();
-    let database = sled::open(DB_FILE_PATH)?;
+    let db = sled::open(DB_FILE_PATH)?;
+    let db = Arc::new(db);
+
+    #[cfg(debug_assertions)] // only delete socket file in development use
+    std::fs::remove_file(DB_SOCKET_PATH).unwrap();
+
     let listener = UnixListener::bind(DB_SOCKET_PATH)?;
     while let Ok((conn, _)) = listener.accept().await {
+        let db = db.clone();
         tokio::spawn(async move {
             let mut server = ipc::UnixServer::new(1024 * 16, conn);
-            let request = server.get_request::<DbRequest>().await.unwrap();
+            let req = server.get_request::<DbRequest>().await.unwrap();
+            let response = match process_request(req, &db) {
+                Ok(r) => r,
+                Err(e) => DbResponse::InternalError(e.to_string()),
+            };
+            server.send_response(&response).await.unwrap();
         });
     }
     Ok(())
-}
-fn table_tree_name(owner: &str, table: &str) -> String {
-    format!("{}:{}", owner, table)
-}
-fn table_tree(owner: &str, table: &str, db: &Db) -> anyhow::Result<sled::Tree> {
-    let tree = db.open_tree(table_tree_name(owner, table))?;
-    Ok(tree)
-}
-fn process_request(req: DbRequest, db: &Db) -> anyhow::Result<DbResponse> {
-    match req {
-        DbRequest::Del { owner, table, key } => {
-            let table_tree = table_tree(&owner, &table, db)?;
-            table_tree.remove(key)?;
-            Ok(DbResponse::DeletedOk)
-        }
-        DbRequest::DelPrefixed {
-            owner,
-            table,
-            prefix,
-        } => {
-            let table_tree = table_tree(&owner, &table, db)?;
-            let prefix_iter = table_tree
-                .scan_prefix(prefix)
-                .filter_map(|r| r.ok())
-                .map(|tuple| tuple.0)
-                .filter(|key| key.as_ref().starts_with(prefix.as_bytes()))
-                .collect::<Vec<_>>();
-            let counter = table_tree
-                .transaction::<_, _, ()>(|db| {
-                    let mut counter = 0u64;
-                    for key in &prefix_iter {
-                        db.remove(key).unwrap();
-                        counter += 1;
-                    }
-                    Ok(counter)
-                })
-                .unwrap();
-            Ok(DbResponse::DeletedManyOk(counter))
-        }
-        DbRequest::Get { owner, table, key } => {
-            let table_tree = table_tree(&owner, &table, db)?;
-            let value = table_tree.get(key).unwrap().map(|value| value.to_vec());
-            Ok(DbResponse::Value(value))
-        }
-        DbRequest::GetPrefixed {
-            owner,
-            table,
-            prefix,
-        } => {
-            let table_tree = table_tree(&owner, &table, db)?;
-            let values = table_tree
-                .scan_prefix(prefix)
-                .filter_map(|r| r.ok())
-                .map(|(_key, val)| val.to_vec())
-                .collect::<Vec<_>>();
-            Ok(DbResponse::ManyValues(values))
-        }
-        DbRequest::Set {
-            owner,
-            table,
-            key,
-            value,
-        } => {
-            let table_tree = table_tree(&owner, &table, db)?;
-            table_tree.insert(key, value).unwrap();
-            Ok(DbResponse::SetOk)
-        }
-        DbRequest::SetMany {
-            owner,
-            table,
-            pairs,
-        } => {
-            let table_tree = table_tree(&owner, &table, db)?;
-            let mut batch = sled::Batch::default();
-            for (key, value) in pairs.into_iter() {
-                batch.insert(key.as_str(), value);
-            }
-            table_tree.apply_batch(batch).unwrap();
-            Ok(DbResponse::SetManyOk)
-        }
-    }
 }
