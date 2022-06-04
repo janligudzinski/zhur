3873cebea326e990b6963f323544f8f8779dcf6d
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-04 16:44:21 +0200

    server loop

--- apst/src/main.rs
+++ apst/src/main.rs
@@ -1,9 +1,13 @@
+use std::sync::Arc;
+
 use common::apst::*;
 use common::errors::IpcError;
 use common::prelude::tokio::net::UnixListener;
 use common::prelude::*;
 use ipc::UnixServer;
 use log::*;
+
+use crate::data::AppStore;
 const STORE_SOCKET_PATH: &str = "/tmp/zhur-apst.sck";
 const STORE_DB_PATH: &str = "/tmp/zhur-apst.sled";
 
@@ -14,22 +18,34 @@ async fn main() -> anyhow::Result<()> {
     info!("Starting app store server.");
     info!("Opening database...");
     let db = sled::open(STORE_DB_PATH)?;
+    let app_store = Arc::new(AppStore::new(db));
     let listener = UnixListener::bind(STORE_SOCKET_PATH)?;
     while let Ok((stream, _)) = listener.accept().await {
-        let mut server = UnixServer::new(1024 * 1024 * 20, stream);
-        loop {
-            let req: AppStoreRequest = match server.get_request().await {
-                Ok(r) => r,
-                Err(IpcError::ClientDisconnected) => {
-                    info!("Client disconnected.");
-                    break;
+        let app_store = app_store.clone();
+        tokio::spawn(async move {
+            let mut server = UnixServer::new(1024 * 1024 * 20, stream);
+            loop {
+                let req: AppStoreRequest = match server.get_request().await {
+                    Ok(r) => r,
+                    Err(IpcError::ClientDisconnected) => {
+                        info!("Client disconnected.");
+                        break;
+                    }
+                    Err(e) => {
+                        warn!("Error while reading request to App Store: {}", e);
+                        break;
+                    }
+                };
+                let res = app_store.handle_request(req);
+                match server.send_response(&res).await {
+                    Ok(_) => continue,
+                    Err(e) => {
+                        error!("Error while sending response from App Store: {}", e);
+                        break;
+                    }
                 }
-                Err(e) => {
-                    warn!("Error while reading request to App Store: {}", e);
-                    break;
-                }
-            };
-        }
+            }
+        });
     }
     Ok(())
 }
