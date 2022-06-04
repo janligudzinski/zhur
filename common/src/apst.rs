c0861f816d4596619cb839613ebbfebfd0a40f67
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-04 15:29:56 +0200

    add variants, AppStoreResponse, ApplicationData

--- common/src/apst.rs
+++ common/src/apst.rs
@@ -1,6 +1,13 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
+pub struct ApplicationData {
+    pub owner: String,
+    pub app_name: String,
+    pub enabled: bool,
+}
+
+#[derive(Deserialize, Serialize)]
 pub enum AppStoreRequest {
     /// Check if an app exists / can be launched.
     AppExists { owner: String, app_name: String },
@@ -16,10 +23,28 @@ pub enum AppStoreRequest {
     DisableApp { owner: String, app_name: String },
     /// Enable a previously disabled app.
     EnableApp { owner: String, app_name: String },
+    /// Rename an app.
+    RenameApp {
+        owner: String,
+        old_name: String,
+        new_name: String,
+    },
     /// Get the code for an app.
     GetAppCode { owner: String, app_name: String },
     /// Get the metadata for all apps for a given user.
     GetOwnedApps { owner: String },
     /// Request state updates since last request made. This message type was implemented mostly so as not to have to implement pub/sub updates.
     RequestUpdates,
+}
+
+#[derive(Deserialize, Serialize)]
+pub enum AppStoreResponse {
+    AppExistence(bool),
+    AppUpserted,
+    AppRemoved,
+    AppRenamed(bool),
+    AppDisabled,
+    AppEnabled,
+    Code { code: Vec<u8> },
+    AppsChanged { apps: Vec<ApplicationData> },
 }
