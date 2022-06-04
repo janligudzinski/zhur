dd679b4b13ce1c5adf146ea63b2daae095db0807
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-04 16:40:48 +0200

    add update handling functions

--- apst/src/data.rs
+++ apst/src/data.rs
@@ -10,6 +10,23 @@ impl AppStore {
     pub fn new(db: Db) -> Self {
         Self { db }
     }
+    fn get_updates(&self) -> Vec<(String, String)> {
+        let tree = self.db.open_tree("updates").unwrap();
+        let mut vals = vec![];
+        for (_key, bytes) in tree.iter().filter_map(|r| r.ok()) {
+            let owner_name = deserialize::<(String, String)>(&bytes).unwrap();
+            vals.push(owner_name);
+        }
+        vals
+    }
+    fn register_update(&self, owner: &str, app_name: &str) {
+        let tree = self.db.open_tree("updates").unwrap();
+        tree.insert(
+            self.db.generate_id().unwrap().to_le_bytes(),
+            serialize(&(owner, app_name)).unwrap(),
+        )
+        .unwrap();
+    }
     fn app_exists(&self, owner: &str, app_name: &str) -> (bool, bool) {
         let tree = self.db.open_tree(owner).unwrap();
         let app_data = tree.get(app_name).unwrap();
