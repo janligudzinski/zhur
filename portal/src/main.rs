ab2a4a5a27e1ccfe10a90e6e3e1946388abdad18
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-05 14:13:41 +0200

    serves register and login

--- portal/src/main.rs
+++ portal/src/main.rs
@@ -1,5 +1,22 @@
+use std::{net::SocketAddr, str::FromStr, sync::Arc};
+
+use axum::{routing::post, Extension};
+use common::prelude::*;
 mod users;
+use users::{data::UserRepo, web as user_routes};
 const USERS_DB_PATH: &str = "/tmp/zhur-users.sled";
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let db = sled::open(USERS_DB_PATH).unwrap();
+    let repo = Arc::new(UserRepo::new(db));
+    let router = axum::Router::new()
+        .route("/register", post(user_routes::register))
+        .route("/login", post(user_routes::login))
+        .layer(Extension(repo))
+        .into_make_service();
+
+    axum::Server::bind(&SocketAddr::from_str("0.0.0.0:8001").unwrap())
+        .serve(router)
+        .await
+        .unwrap();
 }
