6744ce8125b6fa056d7608be2927335d71c6807d
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-06-05 14:06:59 +0200

    login

--- portal/src/users/web.rs
+++ portal/src/users/web.rs
@@ -1,13 +1,27 @@
 use super::data::*;
 use axum::{http::StatusCode, response::IntoResponse, Extension, Json};
 use common::prelude::*;
+use hmac::{Hmac, Mac};
+use jwt::SignWithKey;
 use serde::{Deserialize, Serialize};
+use sha2::Sha256;
 use std::sync::Arc;
+const HMAC_KEY: &[u8; 24] = b"DO NOT USE IN PRODUCTION";
 
 #[derive(Deserialize, Serialize)]
 pub struct RegisterRequest {
-    name: String,
-    password: String,
+    pub name: String,
+    pub password: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct LoginClaims {
+    pub sub: String,
+}
+#[derive(Deserialize, Serialize)]
+pub struct LoginRequest {
+    pub name: String,
+    pub password: String,
 }
 
 pub async fn register(
@@ -21,5 +35,22 @@ pub async fn register(
             StatusCode::UNPROCESSABLE_ENTITY,
             Json("User not registered - the username was already taken."),
         )
+    }
+}
+
+pub async fn login(
+    Extension(repo): Extension<Arc<UserRepo>>,
+    Json(req): Json<LoginRequest>,
+) -> impl IntoResponse {
+    if repo.login(&req.name, &req.password) {
+        let claim = LoginClaims { sub: req.name };
+        let key = Hmac::<Sha256>::new_from_slice(HMAC_KEY).unwrap();
+        let token_str = claim.sign_with_key(&key).unwrap();
+        Ok((StatusCode::OK, Json(token_str)))
+    } else {
+        Err((
+            StatusCode::NOT_FOUND,
+            Json("Invalid username/password combination."),
+        ))
     }
 }
